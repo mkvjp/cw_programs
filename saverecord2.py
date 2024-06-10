7f7fd90f512ee810978938774874d08c03c525bc
mkv <kvmatsu@gmail.com>
2024-06-10 20:08:43 +0900

    update

---
+++ saverecord2.py
@@ -0,0 +1,31 @@
+# imports libs
+import pandas as pd
+import datetime
+import serial
+import os
+
+
+def main(baudrate:int = 9600):
+    # sets comport number and baudrate
+    comname = "/dev/tty.usbmodem141201" #winならcom13とか
+    com = serial.Serial(comname,baudrate)
+    os.makedirs(f"record",exist_ok=True)
+    foldername = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    os.makedirs(f"record/"+foldername, exist_ok=True)
+    df = pd.DataFrame(columns=['timestamp','voltage'])
+    df = df.set_index("timestamp",drop=True)
+    while True:
+
+        file_name = "record/"+foldername+"/record.csv"
+        print("start!")
+        while True:
+            # gets now time
+            
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            voltage = str(com.readline().decode("utf-8"))
+            df.loc[now] = [voltage]
+            df.to_csv(file_name)
+            print(voltage)
+        
+if __name__ == '__main__':
+    main()
